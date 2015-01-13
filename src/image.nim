43f550eafddbd380ce14e3e32b64d3eee4919e44
Jonathan Sudlow <jsudlow@gmail.com>
2015-01-13 14:40:51 -0600

    Encapsulated map reading into readMap(pathToJSON) so its now importable and easily runnable

--- src/image.nim
+++ src/image.nim
@@ -5,3 +5,13 @@ type
     trans*: string
     width*: int
     height*: int
+
+proc newImage*(format:string, source:string,trans:string,width:int,height:int ): ref Image =
+  result = new Image
+  result.format = format
+  result.source = source
+  result.trans = trans
+  result.width = width
+  result.height = height
+
+  
--- tiled.nim
+++ tiled.nim
@@ -1,16 +1,20 @@
 import src/map, src/layer, src/tileset, src/image, src/tile, streams,hashes,strutils,lexbase,unicode,json
 
-#Get the json saved on our disk
-var jsonStreamFromFile = newFilestream("map_files/nimrodhunter.json", fmRead)
 
-#Parse the json
-var jobj = parseJson(jsonStreamFromFile,"error.json")
+proc readMap(pathToJSONFile: string): ref Map =
+  echo "ok"
+  #Get the json saved on our disk
+  var jsonStreamFromFile = newFilestream(pathToJSONFile, fmRead)
 
-#Test to make sure we actually loaded a json object
-assert (jobj.kind == JObject)
+  #Parse the json
+  var jobj = parseJson(jsonStreamFromFile,"error.json")
 
-#Print out a few check values to make sure we got the right stuff
-echo "Attempting to construct map from JSON"
+  #Test to make sure we actually loaded a json object
+  assert (jobj.kind == JObject)
 
-var Map = newMapFromJSON(jobj)
-echo Map.tileHeight
+  #Print out a few check values to make sure we got the right stuff
+  echo "Attempting to construct map from JSON"
+
+  result = newMapFromJSON(jobj)
+var my_map = readMap("map_files/nimrodhunter.json")  
+echo my_map.orientation 
